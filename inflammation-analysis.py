e507f01a288ab74b3b0de27e65258e6c5c624824
VeraBekkers <vera.bekkers@wur.nl>
2025-10-28 15:46:40 +0100

    Code style fixes.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Software for managing and analysing patients' inflammation data in our imaginary hospital."""
+"""Software for managing and analyzing patients' inflammation data in our imaginary hospital."""
 
 import argparse
 
@@ -13,15 +13,16 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
+    in_files = args.infiles
+    if not isinstance(in_files, list):
         InFiles = [args.infiles]
-
 
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data), 
+                     'max': models.daily_max(inflammation_data), 
+                     'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
 
