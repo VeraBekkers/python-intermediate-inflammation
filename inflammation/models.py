72249d9d4a9b03a3dad1b5b9880db1bfadff42be
VeraBekkers <vera.bekkers@wur.nl>
2025-12-01 13:14:02 +0100

    Add documentation with MKDocs

--- inflammation/models.py
+++ inflammation/models.py
@@ -11,9 +11,17 @@ import numpy as np
 
 
 def load_csv(filename):
-    """Load a Numpy array from a CSV
+    """Load a Numpy array from csv
 
-    :param filename: Filename of CSV to load
+        Parameters
+        ----------
+        filename : str
+            path to the csv file
+
+        Returns
+        -------
+        np.ndarray
+            2D array of inflammation data
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
